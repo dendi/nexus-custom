5ce8a59044228360fc083b0a9bf99d5072210c8d
dendi <dendipradigta@gmail.com>
2020-05-28 13:49:45 +0700

    add terraform provider

--- main.tf
+++ main.tf
@@ -1,64 +1,16 @@
-module "gke" {
-  source                     = "terraform-google-modules/kubernetes-engine/google"
-  project_id                 = "<PROJECT ID>"
-  name                       = "gke-test-1"
-  region                     = "us-central1"
-  zones                      = ["us-central1-a", "us-central1-b", "us-central1-f"]
-  network                    = "vpc-01"
-  subnetwork                 = "us-central1-01"
-  ip_range_pods              = "us-central1-01-gke-01-pods"
-  ip_range_services          = "us-central1-01-gke-01-services"
-  http_load_balancing        = false
-  horizontal_pod_autoscaling = true
-  network_policy             = true
+provider "google" {
+#  credentials = "${file("service-account.json")}"
+#  project     = "aliz-hw-dendiparadigta"
+#  region      = "us-central1"
+#  zone        = "us-central1-c"
+#}
 
-  node_pools = [
-    {
-      name               = "default-node-pool"
-      machine_type       = "n1-standard-2"
-      min_count          = 1
-      max_count          = 100
-      local_ssd_count    = 0
-      disk_size_gb       = 100
-      disk_type          = "pd-standard"
-      image_type         = "COS"
-      auto_repair        = true
-      auto_upgrade       = true
-      service_account    = "project-service-account@<PROJECT ID>.iam.gserviceaccount.com"
-      preemptible        = false
-      initial_node_count = 80
-    },
-  ]
-
-  node_pools_oauth_scopes = {
-    all = []
-
-    default-node-pool = [
-      "https://www.googleapis.com/auth/cloud-platform",
-    ]
-  }
-
-  node_pools_labels = {
-    all = {}
-
-    default-node-pool = {
-      default-node-pool = true
-    }
-  }
-
-  node_pools_metadata = {
-    all = {}
-
-    default-node-pool = {
-      node-pool-metadata-custom-value = "my-node-pool"
-    }
-  }
-
-  node_pools_tags = {
-    all = []
-
-    default-node-pool = [
-      "default-node-pool",
-    ]
-  }
+provider "google" {
+  credentials = "${file("service-account.json")}"
+  project     = "dendi-project"
+  region      = "us-central1"
+  zone        = "us-central1-c"
 }
+
+
+
