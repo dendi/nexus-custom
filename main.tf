f8232a332e43b83894867ddf994899d6f0b7ba24
dendi <dendipradigta@gmail.com>
2020-05-28 14:36:17 +0700

    fix(provider): update teraform compatible with v.3x

--- main.tf
+++ main.tf
@@ -1,16 +1,13 @@
 #provider "google" {
-#  credentials = "${file("service-account.json")}"
+#  credentials = file("service-account.json")
 #  project     = "aliz-hw-dendiparadigta"
 #  region      = "us-central1"
 #  zone        = "us-central1-c"
 #}
 
-provider "google" {
-  credentials = "${file("service-account.json")}"
+provider "google" { 
+  credentials = file("service-account.json")
   project     = "dendi-project"
   region      = "us-central1"
   zone        = "us-central1-c"
 }
-
-
-
